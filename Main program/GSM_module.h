ca929bfc92ea56faec105aca330ca55ccff84dcd
amaar711 <amaar711@student.liu.se>
2013-11-25 11:44:54 +0100

    Uppdaterad switch, och lite annat..

--- Main program/GSM_module.h
+++ Main program/GSM_module.h
@@ -11,31 +11,47 @@
 #include <stdio.h>
 #include <string.h>
 
-extern const char ATResponseOK[];
-// Initiating the GSM module
+extern const char responseOK[];
+
 void pinGSM();
 
+//Turn on the GSM module
+void pwrOnOff(void);
+
+//Initiating the GSM module
 void initGSM(void);
 
-// Sends AT command to Tx buffer
-void sendATCommand(int length, const char *command);
+//Sends AT command to Tx buffer
+void sendATCommand(const char *command);
 
-//Send stop bit to GSM module, to end task
+//Check if GSM returns "OK"
+void checkAT();
+
+//Check if GSM has registered on network
+char checkRegistration(char *message);
+
+//Read SMS from GSM module
+char readSMS();
+
+//Sifts out the SMS from the total message from the GSM module
+void whatIsTheSMS(char* c);
+
+//Search for the beginning of SMS
+char searchForSMS(char *SMS);
+
+//Send stop bit to GSM module, to end task (use only when sending SMS)
 void sendCtrlZ(void);
+
+//
+void sendSMS(char *SMS);
+
+//
+void deleteSMS();
 
 //Compare response from GSM to wanted response
 int compareEasy(char *response, const char *wantedResponse);
 
-char readSMS();
+void writeFlashLowTolerance(int lowTreshold);
 
-int searchForSMS(char *message);
-
-void doSomethingDummy();
-
-void pwrOn(void);
-void checkAT();
-void whatIsTheMessage();
-
-void sendSMS(char *message);
-
+void writeFlashHighTolerance(int highTreshold);
 #endif /* GSM_MODULE_H_ */
