319cd78bb1a76e661f1ad041c17316f916093e96
amaar711 <amaar711@student.liu.se>
2013-12-04 19:41:53 +0100

    Städat lite

--- Main program/GSM_module.h
+++ Main program/GSM_module.h
@@ -16,10 +16,10 @@ extern const char responseOK[];
 void pinGSM();
 
 //Turn on the GSM module
-void pwrOnOff(void);
+void pwrOnOff();
 
 //Initiating the GSM module
-void initGSM(void);
+void initGSM();
 
 //Sends AT command to Tx buffer
 void sendATCommand(const char *command);
@@ -29,36 +29,5 @@ void checkAT();
 
 //Check if GSM has registered on network
 char checkRegistration(char *message);
-
-char checkNumber(char *phoneList, char *number, int pos);
-
-void sendNumber(char *number);
-
-//Read SMS from GSM module
-char readSMS();
-
-//Sifts out the SMS from the total message from the GSM module
-void whatIsTheSMS(char* c);
-
-//Search for the beginning of SMS
-char searchForSMS(char *SMS);
-
-//Send stop bit to GSM module, to end task (use only when sending SMS)
-void sendCtrlZ(void);
-
-//
-void sendSMS(char *SMS);
-
-void sendAlarm(char *SMS, int value);
-
-void responseStatus(char *SMS, int sensor);
-
-void responseLvlChange(char *SMS, int offset);
-
-void responseThChange(char *SMS, int lower, int upper);
-
-void responseNrChange(char *SMS);
-
-void deleteSMS();
 
 #endif /* GSM_MODULE_H_ */
